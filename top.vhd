f8790f3d814031d4d72b625f731fe8c69835c8aa
DerQut2 <275818@student.pwr.edu.pl>
2025-06-03 18:59:31 +0200

    Dodano flagi zdarzen oraz ustawianie pozycji kursora

--- top.vhd
+++ top.vhd
@@ -71,13 +71,19 @@ architecture top_arch of top is
 	signal confirm_button_buffer: std_logic := '0';
 	-- --
 	--
+	-- -- Flagi zdarzen przyciskow
+	signal left_button_event: std_logic := '0';
+	signal right_button_event: std_logic := '0';
+	signal confirm_button_event: std_logic := '0';
+	-- --
+	--
 	-- -- Aktualna tura oraz stan planszy
 	signal game_turn: GAME_t := 'X';
 	signal game_reg: GAME_t_Vector := "---------";
 	-- --
 	--
 	-- -- Aktualna pozycja kursora
-	signal game_pos: unsigned (3 downto 0) := (others => '0');
+	signal cursor_pos: unsigned (3 downto 0) := (others => '0');
 	
 	
 	
@@ -116,12 +122,66 @@ begin
 	-- --
 	BUUFER_BUTTONS: process(Clock100MHz, left_button, right_button, confirm_button)
 	begin
-		if (rising_edge(Clock100MHz)) then
+		if (falling_edge(Clock100MHz)) then
 			left_button_buffer <= left_button;
 			right_button_buffer <= right_button;
 			confirm_button_buffer <= confirm_button;
 		end if;
 	end process BUFFER_BUTTONS;
+	-- --
+	--
+	-- -- Kombinacyjne ustawienie flag zdarzen przyciskow
+	--
+	-- -- -- Odczyt:
+	-- -- -- -- left_button: std_logic
+	-- -- -- -- right_button: std_logic
+	-- -- -- -- confirm_button: std_logic
+	-- -- -- -- left_button_buffer: std_logic
+	-- -- -- -- right_button_buffer: std_logic
+	-- -- -- -- confirm_button_buffer: std_logic
+	-- -- -- --
+	-- --
+	-- -- -- Zapis:
+	-- -- -- -- left_button_event: std_logic
+	-- -- -- -- right_button_event: std_logic
+	-- -- -- -- confirm_button_event: std_logic
+	-- --
+	SET_BUTTON_EVENT_FLAGS: process(left_button, right_button, confirm_button, left_button_buffer, right_button_buffer, confirm_button_buffer)
+	begin
+		left_button_event <= '1' when (left_button = '1' AND left_button_buffer = '0') else '0';
+		right_button_event <= '1' when (right_button = '1' AND right_button_buffer = '0') else '0';
+		confirm_button_event <= '1' when (confirm_button = '1' AND confirm_button_buffer = '0') else '0';
+	end process SET_BUTTON_EVENT_FLAGS;
+	-- --
+	--
+	-- -- Sekwencyjne ustawienie pozycji kursora
+	--
+	-- -- -- Odczyt:
+	-- -- -- -- Clock100MHz: std_logic
+	-- -- -- -- left_button_event: std_logic
+	-- -- -- -- right_button_event: std_logic
+	-- -- -- -- confirm_button_event: std_logic
+	-- -- -- -- cursor_pos: unsigned (3 downto 0)
+	-- --
+	-- -- -- Zapis:
+	-- -- -- -- cursor_pos: unsigned (3 downto 0)
+	-- --
+	SET_CURSOR_POSITION: process(Clock100MHz, left_button_event, right_button_event, confirm_button_event, cursor_pos)
+	begin
+		if (rising_edge(Clock100MHz)) then
+			if (left_button_event = '1') then
+				if NOT(cursor_pos = "0000") then
+					cursor_pos <= cursor_pos - 1;
+				end if;
+			end if;
+			
+			if (right_button_event = '1') then
+				if NOT(cursor_pos = "1000") then
+					cursor_pos <= cursor_pos + 1;
+				end if;
+			end if;
+		end if;
+	end process SET_CURSOR_POSITION;
 	
 
 
