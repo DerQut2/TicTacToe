cee4ee10c80c1c880cb73cc071edc6a73fcd204e
DerQut2 <275818@student.pwr.edu.pl>
2025-06-05 08:36:02 +0200

    Dodano przykładową obsługę vga

--- top.vhd
+++ top.vhd
@@ -94,6 +94,17 @@ architecture top_arch of top is
 	signal reset: std_logic := '0';
 	signal reset_counter: unsigned (26 downto 0) := (others => '0');
 	
+	-- Skopiowany kod
+	signal clk50: std_logic := '0';
+	signal clk25: std_logic := '0';
+	signal hs : unsigned (9 downto 0);
+	signal vs : unsigned (9 downto 0);
+	signal hs_out: std_logic := '0';
+	signal vs_out: std_logic := '0';
+	signal red: std_logic := '0';
+	signal blue: std_logic := '0';
+	signal green: std_logic := '0';
+	
 	
 	
 begin
@@ -210,6 +221,315 @@ begin
 	LED(3) <= confirm_button;
 	--
 	Buzzer <= reset;
+	
+	-- generate a 50Mhz clock
+ 
+	process (Clock100MHz)
+	 
+	begin
+	 
+		if Clock100MHz'event and Clock100MHz='1' then
+		 
+		if (clk50 = '0') then             
+		 
+		clk50 <= '1';
+		 
+		else
+		 
+		clk50 <= '0';
+		 
+		end if;
+		 
+		end if;
+	 
+	end process;
+	
+	process (clk50)
+	 
+	begin
+	 
+		if clk50'event and clk50='1' then
+		 
+		if (clk25 = '0') then             
+		 
+		clk25 <= '1';
+		 
+		else
+		 
+		clk25 <= '0';
+		 
+		end if;
+		 
+		end if;
+	 
+	end process;
+	
+	process (clk25)
+ 
+	begin
+ 
+		if clk25'event and clk25 = '1' then
+		 
+		if hs = "0011001000" and vs >= "0011001000" and vs <= "0011111010" then ---horizantal and vertical line display constraint
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0011001000" and vs >= "0100101100" and vs <= "0101000101" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0011111010" and vs >= "0011001000" and vs <= "0011100001" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0011111010" and vs >= "0101000101" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0100000100" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0100000100" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0100110110" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0100110110" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0101000000" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0101000000" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0101110010" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0101111110" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0110101110" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0110010101" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0110111000" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0111010001" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "0111110100" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "1000001101" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "1000110000" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "1001100010" and vs >= "0011001000" and vs <= "0011111010" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "1000110000" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "1001100010" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "1001101100" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		elsif hs = "1010011110" and vs >= "0100101100" and vs <= "0101011110" then
+		 
+		red <= '1' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		--------------------------------------------------------------------------------
+		 
+		else                     ----------blank signal display
+		 
+		red <= '0' ;
+		 
+		blue <= '0';
+		 
+		green <= '0' ;
+		 
+		end if;
+		 
+		if (hs > "0000000000" )
+		 
+		and (hs < "0001100001" ) -- 96+1   -----horizontal tracing
+		 
+		then
+		 
+		hs_out <= '0';
+		 
+		else
+		 
+		hs_out <= '1';
+		 
+		end if;
+		 
+		if (vs > "0000000000" )
+		 
+		and (vs < "0000000011" ) -- 2+1   ------vertical tracing
+		 
+		then
+		 
+		vs_out <= '0';
+		 
+		else
+		 
+		vs_out <= '1';
+		 
+		end if;
+		 
+		hs <= hs + 1 ;
+		 
+		if (hs= "1100100000") then     ----incremental of horizontal line
+		 
+		vs <= vs + 1;       ----incremental of vertical line
+		 
+		hs <= "0000000000";
+		 
+		end if;
+		 
+		if (vs= "1000001001") then                
+		 
+		vs <= "0000000000";
+		 
+		end if;
+		 
+		end if;
+		 
+		end process;
+		
+		VGA_R <= red;
+		VGA_B <= blue;
+		VGA_G <= green;
+		-- -- Linie synchronizacji
+		VGA_HS <= hs_out;
+		VGA_VS <= vs_out;
+	
+	
 	
 
 
